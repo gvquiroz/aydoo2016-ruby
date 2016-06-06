881f518ac6a26f19cf7869afd6ebf5a919878c38
gvquiroz <gvquiroz@gmail.com>
2016-06-06 19:10:27 -0300

    Agregado formateador pretty y quiett

--- spec/formateador_quiett_spec.rb
+++ spec/formateador_quiett_spec.rb
@@ -6,7 +6,7 @@ class FormateadorQuiettSpec
     it 'should return 2 2 3 5 when get contenido formateado' do
       array_con_contenido = [2, 2, 3, 5]
       expected_string = "2\n2\n3\n5"
-      formateador = FormateadorPretty.new
+      formateador = FormateadorQuiett.new
       expect(formateador.get_contenido_formateado(array_con_contenido)).to eq(expected_string)
     end
 
