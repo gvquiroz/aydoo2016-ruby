8aca6b5286121394c908f0384c3099c96e2a6015
gvquiroz <gvquiroz@gmail.com>
2016-06-06 22:59:35 -0300

    refactor

--- spec/gestor_de_salida_spec.rb
+++ spec/gestor_de_salida_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require_relative '../model/gestor_de_salida'
 class GestorDeSalidaSpec
+  #requiere la version de spec de 3.0 en adelante para que tome output().to_stdout
   describe 'GestorDeSalida' do
     it 'should imprimir por pantalla when imprimir por pantalla' do
       contenido_de_prueba = "mi contenido de prueba"
