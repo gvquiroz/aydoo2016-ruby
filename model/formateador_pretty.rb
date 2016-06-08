d8138db6f8f7e206bc7f5448f05a225a3a71d3d9
gvquiroz <gvquiroz@gmail.com>
2016-06-07 23:32:45 -0300

    Agregado build_all y estructura de proyecto

--- model/formateador_pretty.rb
+++
@@ -1,9 +0,0 @@
-class FormateadorPretty
-  def get_contenido_formateado (miArray)
-    @resultado = ""
-    miArray.each do |valor|
-      @resultado += "#{valor} "
-    end
-    @resultado.chop!
-  end
-end
--- model/formateador_quiett.rb
+++
@@ -1,9 +0,0 @@
-class FormateadorQuiett
-  def get_contenido_formateado (miArray)
-    @resultado = ""
-    miArray.each do |valor|
-      @resultado += "#{valor}\n"
-    end
-    @resultado.chop!
-  end
-end
